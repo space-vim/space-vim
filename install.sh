66c0cba45e6949c0aedc1fbd3c88e195a63fda79
Liucheng.Xu <xuliuchengxlc@users.noreply.github.com>
2016-05-01 13:03:29 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-echo "It will take some time to finsh the setup..., please be patient"
+
+echo "整个安装过程可能需要花费一些时间，请耐心等待......"
 
 if which apt-get >/dev/null; then
 	sudo apt-get install -y vim git
@@ -17,22 +18,25 @@ if [ -d "~/.vim" ]; then
 	mv -f ~/.vim ~/.vim_old
 fi
 
-if [ -f "~/.dotfile" ]; then
-	mv -f ~/.dotfile ~/.dotfile_old
-fi
-
 ####  setup vundle for vim
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
-git clone https://github.com/xuliuchengxlc/dotfile.git ~/.dotfile
+git clone https://github.com/xuliuchengxlc/vim.git ~/.vim/dotfile
 
-ln -s ~/.dotfile/vim/.vimrc ~/.vimrc
+ln -s ~/.vim/dotfile/.vimrc ~/.vimrc
 
-echo "***************************************" > xlc
-echo "vundle正在安装插件......" >> xlc
-echo "可能需要一些时间来完成所有插件的安装，" >> xlc
-echo "请耐心等待......" >> xlc
-echo "***************************************" >> xlc
+echo "********************************************** "       > xlc
+echo "********************************************** "      >> xlc
+echo "vundle正在安装插件......                       "      >> xlc
+echo "可能需要一些时间来完成所有插件的安装，         "      >> xlc
+echo "请耐心等待......, 安装完成后会自动退出。       "      >> xlc
+echo "********************************************** "      >> xlc
+echo "********************************************** "      >> xlc
+echo "安装完成后须在terminal的设置中进行相关设置,    "      >> xlc
+echo "即将原有字体改为带有“for powerline”的字体。    "      >> xlc
+echo "否则airline会出现字符乱码，无法正常显示的情况。"      >> xlc
+echo "********************************************** "      >> xlc
+echo "********************************************** "      >> xlc
 
 #### install plugins
 vim xlc -c "PluginInstall" -c "q" -c "q"
@@ -47,7 +51,4 @@ fi
 git clone https://github.com/powerline/fonts.git ~/.fonts
 sh ~/.fonts/install.sh
 
-echo "*********     就快完成啦！    *********" 
-echo "现在只须在terminal的字体设置中将字体样式更改为带有“for powerline”的即可。"
-echo "否则airline可能会出现字符乱码，无法正常显示的情况。"
-echo "***************************************"
+
