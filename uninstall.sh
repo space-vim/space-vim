ade5dd9413eedb04d6e13e6fd471397c3d88ec78
Liucheng.Xu <xuliuchengxlc@users.noreply.github.com>
2016-05-02 07:45:40 +0800

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-rm -f $HOME/.vimrc
-rm -rf $HOME/.vim
+rm -f ${HOME}/.vimrc
+rm -rf ${HOME}/.vim
 
 
-if [ -f "$HOME/.vimrc_old" ]; then
-	mv -f $HOME/.vimrc_old $HOME/.vimrc
+if [ -f "${HOME}/.vimrc_old" ]; then
+	mv -f ${HOME}/.vimrc_old ${HOME}/.vimrc
 fi
 
-if [ -d "$HOME/.vim_old" ]; then
-	mv -f $HOME/.vim_old $HOME/.vim
+if [ -d "${HOME}/.vim_old" ]; then
+	mv -f ${HOME}/.vim_old ${HOME}/.vim
 fi
 
 
