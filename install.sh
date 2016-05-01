bb1e8ed9fe74e852f6ebadb88d5e3d8968af7f82
xuliuchengxlc <xuliuchengxlc@gmail.com>
2016-05-01 10:09:08 +0800

    modified install.sh and add uninstall

--- install.sh
+++ install.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
-echo "installing..."
+echo "It will take some time to finsh the setup..., please be patient"
 
 if which apt-get >/dev/null; then
 	sudo apt-get install -y vim git
 elif which yum >/dev/null; then
 	sudo yum install -y vim git
 fi
+
+cd ~/
+mv -f ~/.vimrc ~/.vimrc_old
+
 
 ####  setup vundle for vim
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
@@ -14,8 +18,10 @@ git clone https://github.com/xuliuchengxlc/dotfile.git ~/.dotfile
 
 ln -s ~/.dotfile/vim/.vimrc ~/.vimrc
 
-echo "installing vim plugins"
+echo "installing plugins" > xlc
+echo "It will quit automatically when the whole plugins are installed" >> xlc
+echo "Please be patient..." >> xlc
 vim xlc -c "PluginInstall" -c "q" -c "q"
 rm xlc
 
-echo "finshed"
+echo "done!"
---
+++ uninstall.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+rm -f ~/.vimrc
+rm -rf ~/.vim
+
+mv -f ~/.vimrc_old ~/.vimrc
+
