8a197bc837d2c8ea7d78d70d311555877fee11f5
Liucheng.Xu <xuliuchengxlc@users.noreply.github.com>
2016-05-01 12:27:53 +0800

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -2,6 +2,19 @@
 
 rm -f ~/.vimrc
 rm -rf ~/.vim
+rm -rf ~/.dotfile
 
-mv -f ~/.vimrc_old ~/.vimrc
+
+if [ -f "~/.vimrc_old" ]; then
+	mv -f ~/.vimrc_old ~/.vimrc
+fi
+
+if [ -d "~/.vim_old" ]; then
+	mv -f ~/.vim_old ~/.vim
+fi
+
+if [ -f "~/.dotfile_old" ]; then
+	mv -f ~/.dotfile_old ~/.dotfile
+fi
+
 
